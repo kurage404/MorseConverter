882ff0123753f27cc693e996c2459eeb8aeae058
kurage404 <kurage404@gmail.com>
2021-07-14 23:40:12 +0900

    Organize process

--- tools/morse_translator.py
+++ tools/morse_translator.py
@@ -23,16 +23,33 @@ def get_translate_list(lang):
         print("File Does Not Exist")
         return False
 
-def message_check(m):
-    traint=1
-    return tra_int
-
-def translate_message(m,tra_list):
-    tra_int = message_check(m)
-    tra_mes = ""
+def message_check(m,tra_list):
+    morse_tra,message_tra = 0,0
     for i in m:
         for li in tra_list:
             if(i in li):
-                print(li)
-                tra_mes = tra_mes + li[tra_int] + " "
+                if(li.index(i) == 1):
+                    morse_tra += 1
+                else:
+                    message_tra += 1
+    if(morse_tra > message_tra):
+        tra_int = 1
+    else:
+        tra_int = 0
+    return tra_int
+
+def translate_message(m,lang):
+    tra_list = get_translate_list(lang)
+    tra_int = message_check(m,tra_list)
+    tra_mes = ""
+    if (tra_int = 0):
+        for i in m:
+            for li in tra_list:
+                if(i in li):
+                    tra_mes = tra_mes + li[1] + " "
+    else:
+        for i in m:
+            for li in tra_list:
+                if(i in li):
+                    tra_mes = tra_mes + li[0]
     print(tra_mes)
