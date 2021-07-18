f93acb28a7f6995fc661953c462714d70176d64e
kurage404 <kurage404@gmail.com>
2021-07-18 23:57:58 +0900

    wrote morse_config

--- tools/morse_converter.py
+++ tools/morse_converter.py
@@ -1,7 +1,8 @@
 # code:Python3/UTF-8
 # made by kurage404
+
 def defult_str():
-    return("・","－","0","1")
+    return("ăť","ďź","0","1")
 
 def convert(from1, from2, to1, to2, m):
     m = m.replace(from1, to1)
